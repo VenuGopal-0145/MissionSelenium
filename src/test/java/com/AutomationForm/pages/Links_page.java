28be6ef82ebef1de71df90950c0dcdee4cd8d9c1
721326 <721326@LTIN456650.cts.com>
2024-11-18 21:50:05 +0530

    Link_page

--- src/test/java/com/AutomationForm/pages/Links_page.java
+++ src/test/java/com/AutomationForm/pages/Links_page.java
@@ -1,5 +1,55 @@
 package com.AutomationForm.pages;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Links_page {
+	public WebElement Home_new,Home_Pwp,Get_code,Api_201,Api_204,Api_301,Api_400,Api_401,Api_403,Api_404;
+	public String urlString="https://www.tutorialspoint.com/selenium/practice/links.php";
+	
+	public void linkPage() throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.navigate().to(urlString);
+		driver.manage().window().maximize();
+		
+		Home_new=driver.findElement(By.linkText("Home"));
+		Home_Pwp=driver.findElement(By.partialLinkText("PWP"));
+		Api_201=driver.findElement(By.id("created"));
+		Api_204=driver.findElement(By.id("no-content"));
+		Api_301=driver.findElement(By.id("moved"));
+		Api_400=driver.findElement(By.id("bad-request"));
+		Api_401=driver.findElement(By.id("unauthorized"));
+		Api_403=driver.findElement(By.id("forbidden"));
+		Api_404=driver.findElement(By.id("not-found"));
+		List<WebElement> eList=new ArrayList<WebElement>();
+		eList.add(Api_201);
+		eList.add(Api_204);
+		eList.add(Api_301);
+		eList.add(Api_400);
+		eList.add(Api_401);
+		eList.add(Api_403);
+		eList.add(Api_404);
+		
+		 List<WebElement> elements=driver.findElements(By.xpath("//div[@class='col-md-8 col-lg-8 col-xl-8']/div"));
+		 for(WebElement eset:eList )
+		 {
+			 for(WebElement get: elements) {
+				 eset.click();
+				 String valString=get.getText();
+				 if(valString!="")
+					 System.out.println(valString);
+				 else 
+					 continue;
+				// Thread.sleep(4000);
+			 }
+		 }
+		
+	}
 
 }
--- src/test/java/com/AutomationForm/test/TC_Links.java
+++ src/test/java/com/AutomationForm/test/TC_Links.java
@@ -1,9 +1,13 @@
 package com.AutomationForm.test;
 
-public class TC_Links {
+import com.AutomationForm.pages.Links_page;
 
-	public static void main(String[] args) {
+public class TC_Links extends Links_page{
+
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		TC_Links tLinks=new TC_Links();
+		tLinks.linkPage();
 
 	}
 
